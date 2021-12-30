8a9cb9e59d971df67977197605acb45d93e40a54
Tt6588 <tatsusato1011@gmail.com>
2021-12-30 23:30:01 +0900

    [finish]1章

--- 1/demo.java
+++ 1/demo.java
@@ -6,5 +6,19 @@ public class Demo {
 		System.out.println("突然ですが砂糖の合計を計算します");
 		System.out.println(12 + 3 * 6);
 // 		コメント練習
+
+    int age;
+    age = 23;
+    System.out.println(age);
+
+
+    final double PI = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
+    System.out.println("パイの半径を倍にします");
+    PI = 10; //代入できないエラーが出る
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
 	}
 }
