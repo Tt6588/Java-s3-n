8b09a7ca35652e9aa4466d7a65c565f08617ea6d
Tt6588 <tatsusato1011@gmail.com>
2021-12-30 22:26:36 +0900

    [update]demo

--- 1/demo.java
+++ 1/demo.java
@@ -1,5 +1,10 @@
-public class Main {
+public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hello, world!");
+		System.out.println("sugar");
+		System.out.println("正体は角砂糖");
+		System.out.println("だと本人は思っている…");
+		System.out.println("突然ですが砂糖の合計を計算します");
+		System.out.println(12 + 3 * 6);
+// 		コメント練習
 	}
 }
