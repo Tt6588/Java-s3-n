6491561cbb497d3523dc5d1c19b52dd9b6ce8880
Tt6588 <tatsusato1011@gmail.com>
2021-12-31 10:26:35 +0900

    [check]一章

--- 1/demo.java
+++ 1/demo.java
@@ -5,6 +5,7 @@ public class Demo {
 		System.out.println("だと本人は思っている…");
 		System.out.println("突然ですが砂糖の合計を計算します");
 		System.out.println(12 + 3 * 6);
+
 // 		コメント練習
 
     int age;
@@ -20,5 +21,17 @@ public class Demo {
     PI = 10; //代入できないエラーが出る
     System.out.println("半径" + pie + "cmのパイの面積は、");
     System.out.println(pie * pie * PI);
+
+    // 練習問題
+    int height = 3;
+    int width = 5;
+    System.out.println("縦幅" + height + "横幅" + width + "の長方形の面積は、" + height * width);
+    
+    boolean result = true;
+    char faboriteCharactar = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    
 	}
 }
