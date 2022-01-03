a2f6f3334a62e9573788c39d2051078d4e438722
Tt6588 <tatsusato1011@gmail.com>
2022-01-03 23:47:40 +0900

    [start]2章

---
+++ 1/Demo2.java
@@ -0,0 +1,9 @@
+public class Demo2 {
+	public static void main(String[] args) {
+	   // 私の好きな記号は二重引用符が文字列とみなされる
+	    System.out.rintln("私の好きな記号は二重引用符(")です");
+	    
+	   // ￥によつて二重引用符は文字とみなされる
+	    System.out.rintln("私の好きな記号は二重引用符(¥")です");
+	}
+}
