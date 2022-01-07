0af6033b52d2230bee5a13d26fda4749f5164e6b
Tt6588 <tatsusato1011@gmail.com>
2022-01-07 23:47:32 +0900

    2章代入時の自動変換-1

--- 1/Demo3.java
+++ 1/Demo3.java
@@ -5,5 +5,11 @@ public class Demo3 {
     a = 100;
     a++;
     System.out.println(a);
+
+    // コード2-5　代入時の自動型変換　値より大きな方の変数に代入
+    flort f = 3;
+    douvle d = f;
+    System.out.prinln(f);
+    System.out.prinln(d);
   }
 }
