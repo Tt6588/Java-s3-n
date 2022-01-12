0933715a86dc334ee14ebab60596f31183f4a176
Tt6588 <tatsusato1011@gmail.com>
2022-01-12 12:40:04 +0900

    [update]2章

--- 1/Demo3.java
+++ 1/Demo3.java
@@ -7,9 +7,19 @@ public class Demo3 {
     System.out.println(a);
 
     // コード2-5　代入時の自動型変換　値より大きな方の変数に代入
-    flort f = 3;
-    douvle d = f;
-    System.out.prinln(f);
-    System.out.prinln(d);
+    float f = 3;
+    double d = f;
+    System.out.println(f);
+    System.out.println(d);
+    
+    // コード2-6　代入時の自動型変換　値より小さな方の変数に代入（エラー）
+    // int i = 3.2;
+    // System.out.pritnln(i);
+
+    
+    // コード2-7　キャスト演算子（強制的な型変換）
+    int age = (int)3.2;
+    // この場合強制的に型変換を行うため小数点以下が切り捨てられる情報の欠損が起こる
+    System.out.println(age);
   }
 }
