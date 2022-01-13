2573e8e69f043e3b51c435a8387fb6db4b8ccc25
Tt6588 <tatsusato1011@gmail.com>
2022-01-13 19:44:32 +0900

    [update]2章

--- 1/Demo3.java
+++ 1/Demo3.java
@@ -32,9 +32,45 @@ public class Demo3 {
     String msg = "私の年齢は" + 23;
     System.out.println(msg);
     
+    // 2-6
     // コード2-10 命令実行文
     String name = "須賀";
     String message = name + "さん、こんにちは";
     System.out.println(message);
+
+    // コード2-11 改行なし画面出力の命令
+    String name1 = "菅原";
+    System.out.print("私の名前は");
+    System.out.print(name1);
+    System.out.print("です");
+
+    // コード2-12 大きい方の数値を代入する方法
+    int a12 = 5;
+    int b12 = 3;
+    int m12 = Math.max(a12,b12);
+    System.out.println("比較実験:" + a12 +"と" + b12 + "とでは大きい方は…" + m12);
+
+    // コード2-13 String型をint型に変換する命令
+    String age13 = "31";
+    int n13 = Integer.parseInt(age13);
+    System.out.println("あなたは来年、" + (n+1) + "歳になりますね。");
+    
+    // コード2-14 ランダムな数を生成する命令
+    int r14 = new java.util.Random().nextInt(90);
+    System.out.println("あなたはたぶん" + r14 + "歳ですね？");
+    
+    // コード2-15 キーボードから入力を受け付ける命令
+    System.out.println("あなたの名前を入力してください。");
+    int name15 = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    int age15 = new java.util.Scanner(system.in).nextInt();
+    System.out.println("ようこそ、" + age15 + "歳の" + name15 + "さん");
+
+    // 練習問題2-1
+    int x2-1 = 5;
+    int y2-1 = 10;
+    // String ans = "x+yは" + x + y; =x+yは510 → =x+yは15 に修正する
+    String ans = "x+yは" + (x+y);
+    System.out.println(ans);
   }
 }
