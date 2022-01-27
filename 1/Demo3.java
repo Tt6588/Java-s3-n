3d3c77d769b65b58d04b0cbc55d7eb8421c9e26d
Tt6588 <tatsusato1011@gmail.com>
2022-01-27 22:34:26 +0900

    [finish]3章

--- 1/Demo3.java
+++ 1/Demo3.java
@@ -1,6 +1,6 @@
 public class Demo3 {
   public static void main(String[] args) {
-    // インクリメント演算子
+    // 2-4 インクリメント演算子
     int a;
     a = 100;
     a++;
@@ -162,5 +162,86 @@ public class Demo3 {
       case 5:
         System.out.println("だめだこりゃ……");
     }
+
+    // 3-7 基本的なfor文
+    for (int i37 = 0; i37 < 10; i37++){
+      System.out.println("こんにちは");
+    }
+    
+    // 3-8 ループ変数を表示する
+    for (int i38 = 1; i38 < 5; i38++){
+      System.out.print("現在" + i38 + "週目→");
+    }
+
+    // 3-9 for文のネスト
+    for (int i39 = 1; i39 < 10; i39++){
+      for (int j39 = 1; j39 < 10; j39++){
+        System.out.print(i39 * j39);
+        System.out.print(" ");
+      }
+      System.out.println("");  //改行の出力
+    }
+
+    // 練習問題3－1
+    // 1 (weight == 60){
+    // 2 ((age1+age2)*2 > 60){
+    // 3 (age & 2 == 1){
+    // 4 (name.equals("湊")){
+
+    // 練習問題3－3
+    int isHungry = 0;
+    // int isHungry = 1;
+    String food = "リンゴ";
+    System.out.println("こんにちは");
+    if (isHungry == 0){
+      System.out.println("お腹いっぱいです");
+    } else{
+      System.out.println("はらぺこです");
+      System.out.println(food + "をいただきます");
+      System.out.println("ご馳走様でした");
+    }
+
+    // 練習問題3-4
+    boolean tenki34 = true;
+    // boolean tenki34 = false;
+    if (tenki34 == true){
+      System.out.println("洗濯します");
+      System.out.println("散歩に行きます");
+    } else{  //内容が1行でないのに波カッコが表記されていなかった
+      System.out.println("DVDを見ます");
+      System.out.println("寝ます");
+    }
+    
+    // 練習問題3-5
+    System.out.print("[[メニュー]1:検索　2:登録　3:削除　4:変更>]");
+    int selected = new java.util.Scanner(System.in).nextInt();
+    switch (selected){
+      case 1:
+        System.out.println("検索します");
+        break;
+      case 2:
+        System.out.println("登録します");
+        break;
+      case 3:
+        System.out.println("削除します");
+        break;
+      case 4:
+        System.out.println("変更します");
+        break;
+    }
+    
+    // 練習問題3-6
+    System.out.println("【数当てゲーム】");
+    int ans3 = new java.util.Random().nextInt(9);
+    for (int i3 = 0; i3 < 5; i3++){
+      System.out.println("0～9の数字の入力してください");
+      int num3 = new java.util.Scanner(System.in).nextInt();
+      if (ans3 == num3){
+        System.out.println("あたり！");
+      }else {
+        System.out.println("はずれ……");
+      }
+    }
+    System.out.println("ゲームを終了します");
   }
 }
