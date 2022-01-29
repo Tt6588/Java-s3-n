6870461428ab4651f0f8d6fb6df676665a3c8e2b
Tt6588 <tatsusato1011@gmail.com>
2022-01-29 23:58:30 +0900

    [start]4章

---
+++ 1/Demo4.java
@@ -0,0 +1,17 @@
+public class Demo4 {
+  public static void main(String[] args) {
+    
+    // 4-1 点数管理プログラム
+    int sansu = 20;
+    int kokugo = 30;
+    int rika = 40;
+    int eigo = 50;
+    int syakai = 80;
+
+    int sum = sansu + kokugo + rika + eigo + syakai;
+
+    int avg = sum / 5;
+    System.out.println("合計点" + sum);
+    System.out.println("平均点" + avg);
+  }
+}
