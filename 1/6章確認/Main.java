461f28c005bd9b3e0d13fed2e583a25235e1cba9
Tt6588 <tatsusato1011@gmail.com>
2022-02-10 15:32:52 +0900

    [finish]6章

---
+++ 1/6章確認/Main.java
@@ -0,0 +1,11 @@
+// —ûK–â‘è6-1
+import comment.Zenhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan.callDeae();
+    comment.Kouhan.showMondokoro();
+  }
+}
---
+++ 1/6章確認/Zenhan.java
@@ -0,0 +1,11 @@
+// 練習問題6-1
+package comment;
+
+public class Zenhan {
+  public static void doWarusa() {
+    System.out.println("きなこでござる。食えませんがの。");
+  }
+  public static void doTogame() {
+    System.out.println("この老いぼれの目はごまかせませんぞ。");
+  }
+}
