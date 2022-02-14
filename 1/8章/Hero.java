8b1b9478c0011cd58e5dab70f712e13b38cd92e5
Tt6588 <tatsusato1011@gmail.com>
2022-02-14 14:20:25 +0900

    [update]8章

--- 1/8章/Hero.java
+++ 1/8章/Hero.java
@@ -5,4 +5,29 @@ public class Hero {
   // 8-4 heroクラスに名前とHPを変数として宣言
   String name;
   int hp;
+
+  // 8-7 眠る操作に含まれる要素を記述
+  void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は、眠って回復した！");
+  }
+
+  // 8-8 メソッド「座る」「転ぶ」「逃げる」を追加
+  public void sit(int sec) {
+    this.hp += sec;
+    System.out.println(this.name + "は、" + sec + "秒座った！");
+    System.out.println("HPが" + sec + "ポイント回復した");
+  }
+
+  public void slip() {
+    this.hp -= 5;
+    System.out.println(this.name + "は、転んだ！");
+    System.out.println("5のダメージ！");
+  }
+
+  public void run() {
+    System.out.println(this.name + "は、逃げ出した！");
+    System.out.println("GAMEOVER");
+    System.out.println("最終HPは" + this.hp + "でした");
+  }
 }
--- 1/8章/Matango.java
+++ 1/8章/Matango.java
@@ -1,5 +1,6 @@
 // 8-5 フィールドを宣言すると同時に初期値も設定
 public class Matango {
   int hp;
-  int level = 10;
+  // 8-6 フィールドを定数として宣言
+  final int level = 10;
 }
