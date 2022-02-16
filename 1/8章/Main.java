ed287472e40fa5576a0c60e086d8345ce0b63d86
Tt6588 <tatsusato1011@gmail.com>
2022-02-16 13:22:11 +0900

    [update]8章

--- 1/8章/Main.java
+++ 1/8章/Main.java
@@ -1,10 +1,16 @@
 public class Main {
   // 8-1 mainメソッドを作る
   public static void main(String[] args) {
-    // （以下の内容をJavaで記述していく）
-    // 勇者よ、この仮想世界に生まれよ！
-    // お化けキノコよ、この仮想世界に生まれよ！
-    // 勇者よ、戦え！
-    // お化けキノコよ、逃げろ！
+    // 8-9.10.11　勇者を作成
+    Hero h = new Hero;
+    // 8-12 勇者インスタンスを生成して初期値を代入
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
+    // 8-12 仮想世界に勇者を生み出すプログラム
+    h.sit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
   }
 }
